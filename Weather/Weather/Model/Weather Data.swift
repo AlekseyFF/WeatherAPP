db97c15350dc48f2fc4eb0d83cb5f2e1a9e0f8f0
Aleksey Fedorov <83182177+AlekseyFF@users.noreply.github.com>
2021-06-14 20:00:18 +0300

    Add files via upload

---
+++ Weather/Weather/Model/Weather Data.swift
@@ -0,0 +1,23 @@
+//
+//  Weather Data.swift
+//  Weather
+//
+//  Created by Aleksey Fedorov on 12.06.2021.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
